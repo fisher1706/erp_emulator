71f8833ab10b591f62a29437481647b53254e23d
oleg <olazeba@fondy.eu>
2022-12-18 17:44:55 +0200

    first

---
+++ server.py
@@ -0,0 +1,18 @@
+import logging
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+from ilx import ilx_base_mocks, ilx_submit_mocks, ilx_sales_order_mocks
+
+logging.basicConfig(filename='flask.log', level=logging.DEBUG)
+logger = logging.getLogger('flask_server')
+
+
+@app.before_request
+def log_request_info():
+    logger.info(request.get_data())
+
+
+if __name__ == '__main__':
+    app.run(port='8080', debug=True)
